f58bb8ce24f9690648506302e232e16caa224900
JarnKet <ketsadaphone999@gmail.com>
2022-07-14 18:11:54 +0700

    refactor code to functional programming

--- js/rain.js
+++ js/rain.js
@@ -1,35 +1,63 @@
-const rainContainer = document.querySelector('.rain-container');
+(() => {
+	function setUp() {
+		const rainContainer = document.querySelector('.rain-container');
+		const background = 'linear-gradient(transparent, white)';
 
-// background Colors for the raindrop
-const background = 'linear-gradient(transparent, white)';
+		return {
+			rainContainer,
+			background,
+			amount: 100,
+		};
+	}
 
-const amount = 100; // amount of raindops
-let i = 0;
+	function setRainDropProperty() {
+		return {
+			width: Math.random() * 5 + 'px',
+			positionX: Math.floor(Math.random() * window.innerWidth) + 'px',
+			delay: Math.random() * -20 + 's',
+			duration: Math.random() * 5 + 's',
+			opacity: Math.random() + 0.2,
+		};
+	}
 
-// Looping and creating the raindrop then adding to the rainContainer
-while (i < amount) {
-	//  Creating and Element
-	const drop = document.createElement('i');
+	function setDropStyle({
+		drop,
+		width,
+		positionX,
+		delay,
+		duration,
+		background,
+		opacity,
+	}) {
+		drop.style.width = width;
+		drop.style.left = positionX;
+		drop.style.animationDelay = delay;
+		drop.style.animationDuration = duration;
+		drop.style.background = background;
+		drop.style.opacity = opacity;
+	}
 
-	//   CSS Properties for raindrop
-	const raindropProperties = {
-		width: Math.random() * 5 + 'px',
-		positionX: Math.floor(Math.random() * window.innerWidth) + 'px',
-		delay: Math.random() * -20 + 's',
-		duration: Math.random() * 5 + 's',
-		bg: background,
-		opacity: Math.random() + 0.2,
-	};
+	function run() {
+		const { rainContainer, background, amount } = setUp();
 
-	//   Setting Styles for raindrop
-	drop.style.width = raindropProperties.width;
-	drop.style.left = raindropProperties.positionX;
-	drop.style.animationDelay = raindropProperties.delay;
-	drop.style.animationDuration = raindropProperties.duration;
-	drop.style.background = raindropProperties.bg;
-	drop.style.opacity = raindropProperties.opacity;
+		for (let index = 0; index < amount; index++) {
+			const drop = document.createElement('i');
+			const { width, positionX, delay, duration, opacity } =
+				setRainDropProperty();
+			setDropStyle({
+				drop,
+				width,
+				positionX,
+				delay,
+				duration,
+				background,
+				opacity,
+			});
+			rainContainer.appendChild(drop);
+			const i = document.querySelectorAll('i');
+			console.log(i);
+		}
+	}
 
-	//   Appending the raindrop in the raindrop container
-	rainContainer.appendChild(drop);
-	i++;
-}
+	run();
+})();
